131b3b7328268dd1dd0cf677a76d188dbadb7900
Puneeth Gaikwad L <puneeth.gl@elfonze.com>
2024-04-13 17:46:23 +0530

    added timer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 type todoType = { text: string, editing: boolean, completed: boolean, completeBy: string, inProgress: boolean };
-let TodoDef:todoType = {text: "", editing: false, completed: false, completeBy: "", inProgress: false }
+let TodoDef: todoType = { text: "", editing: false, completed: false, completeBy: "", inProgress: false }
 
 @Component({
   selector: 'app-root',
@@ -27,7 +27,9 @@ export class AppComponent {
     if (!this.todos.find(todo => todo.text === newTodo)) {
       let todo: todoType = TodoDef;
       todo.text = this.newTodoText;
-      todo.completeBy = newTodoTime;
+      if (newTodo.length > 0) {
+        todo.completeBy = newTodoTime;
+      }
       this.startTimer(todo);
       this.todos.push(todo);
     } else {
@@ -36,16 +38,6 @@ export class AppComponent {
   }
 
   completed(todo: todoType) {
-    // const now: Date = new Date();
-    // const hours: number = now.getHours();
-    // const minutes: number = now.getMinutes();
-    // // Formatting the time
-    // const formattedHours: string = hours < 10 ? `0${hours}` : `${hours}`;
-    // const formattedMinutes: string = minutes < 10 ? `0${minutes}` : `${minutes}`;
-
-    // const currentTime: string = `${formattedHours}:${formattedMinutes}`;
-
-    // todo.completeBy = currentTime;
     todo.completed = true;
   }
 
@@ -68,39 +60,42 @@ export class AppComponent {
   }
 
   startTimer(todo: todoType) {
-    const timeInput =  this.newTodoTime;
+    const timeInput = this.newTodoTime;
     const currentTime = new Date();
     let timeFormat = JSON.stringify(currentTime).substring(1, 12);
     const inputTime = new Date(`${timeFormat}${timeInput}`);
 
     const timeDiff = inputTime.getTime() - currentTime.getTime();
-    
+
     console.log(inputTime);
     console.log(timeDiff);
     if (timeDiff <= 0) {
       console.error("enter a time in future")
     }
-    
-    let completeBy = "";
 
-    const timerInterval = setInterval(function() {
+
+    const timerInterval = setInterval(function () {
       const currentTime = new Date();
-      const remainingTime = inputTime.getTime() - currentTime.getTime();      
-      if (remainingTime <= 0) {
+      const remainingTime = inputTime.getTime() - currentTime.getTime();
+      if (todo.completed || remainingTime <= 0) {
         clearInterval(timerInterval);
-        todo.completeBy = "Delayed";
-    } else {
-      const hours = Math.floor((remainingTime / (1000 * 60 * 60)) % 24);
-      const minutes = Math.floor((remainingTime / (1000 * 60)) % 60);
-      const seconds = Math.floor((remainingTime / 1000) % 60);
+        if (todo.completed) {
+          todo.completeBy = "";
+        } else {
+          todo.completeBy = "Delayed";
+        }
+      } else {
+        const hours = Math.floor((remainingTime / (1000 * 60 * 60)) % 24);
+        const minutes = Math.floor((remainingTime / (1000 * 60)) % 60);
+        const seconds = Math.floor((remainingTime / 1000) % 60);
 
         console.log(`Time left: ${hours}h ${minutes}m ${seconds}s`);
 
         todo.completeBy = `Complete by ${hours}h ${minutes}m ${seconds}s`;
 
-    }
+      }
     }, 1000)
-    
+
   }
 
 }
